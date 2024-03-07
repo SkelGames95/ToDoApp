4639cb5c53f36fa8058858d3f3ed8e10e9237667
marco.polenta <marco.polenta@doit.it>
2024-03-07 15:07:35 +0100

    Some Components merged

--- src/shared/components/containers/Body.tsx
+++ src/shared/components/containers/Body.tsx
@@ -1,23 +1,35 @@
-import { TasksButtons } from '../buttons/TasksButtons'
-import { Input } from '../sharedComponents/Input'
-import { Schedule } from './Schedule'
-import classes from './style/Body.module.scss'
+import { Schedule } from "./Schedule";
+import classes from "./style/Body.module.scss";
 
 export const Body = () => {
-    return (
-        <div className={classes.bodyCont}>
-            <div>
-                <Schedule />
-                {/* <AddSchedule /> */}
-                
-            </div>
-            <div>
-                <TasksButtons />
-            </div>
-            <div>
-                <Input />
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className={classes.bodyCont}>
+      <div className={classes.scheduleCont}>
+        <Schedule />
+        {/* <AddSchedule /> */}
+      </div>
+      <div>
+        <button
+          className={`${classes.completeButton} ${classes.tasksButton}`}
+        >
+          Complete
+        </button>
+        <button className={`${classes.deleteButton} ${classes.tasksButton}`}>
+          Delete
+        </button>
+        <button
+          className={`${classes.prioritizeButton} ${classes.tasksButton}`}
+        >
+          Prioritize!
+        </button>
+        <button className={`${classes.snoozeButton} ${classes.tasksButton}`}>
+          Snooze
+        </button>
+      </div>
+      <form name="addTask">
+        <input type="text" className={classes.toDoInput} />
+        <button className={classes.addButton}>ADD</button>
+      </form>
+    </div>
+  );
+};
--- src/shared/components/containers/Schedule.tsx
+++ src/shared/components/containers/Schedule.tsx
@@ -1,12 +1,15 @@
 import classes from './style/Schedule.module.scss'
-import { ListTitle } from '../sharedComponents/ListTitle';
+import blackbin from "../../../assets/blackbin.svg";
+import archive from "../../../assets/archive.svg";
 import { TodoList } from './TodoList';
 
 export const Schedule = () => {
   return (
     <div className={classes.schedules}>
-      <div>
-        <ListTitle />
+      <div className={classes.titleCont}>
+      <h2 className={classes.listTitle}>List Title</h2>
+      <img src={blackbin} alt="black bin icon" className={classes.tinyicons} />
+      <img src={archive} alt="archive icon" className={classes.tinyicons} />
       </div>
       <div>
         <TodoList />
